650be7efb263729c7e019ce145a88c121105fdda
Amira Gimeno <amiralorrainegimeno@gmail.com>
2025-11-18 16:37:59 +0800

    ready get set gow

---
+++ app/src/main/java/com/example/orderlythreads/ProductionStaff.kt
@@ -0,0 +1,38 @@
+package com.example.orderlythreads
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ProductionStaff : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_production_staff)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val tab1 = findViewById<TextView>(R.id.tab1)
+        val tab2 = findViewById<TextView>(R.id.tab2)
+        val tab3 = findViewById<TextView>(R.id.tab3)
+        val tab4 = findViewById<TextView>(R.id.tab4)
+
+        val tabs =  listOf(tab1, tab2, tab3, tab4)
+
+        //loop that handles selection of each tab
+        tabs.forEach { tab ->
+            tab.setOnClickListener{
+
+                tabs.forEach { it.isSelected  = false } //deselects the current tab open
+
+                tab.isSelected = true //highlights the selected tab
+            }
+
+        }
+    }
+}
