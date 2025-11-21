7d48a6b2814de1599214fb8fc0fa03b36825aff2
Jerp010 <jeffmarquez2006@gmail.com>
2025-11-21 13:12:20 +0800

    testing

--- app/src/main/java/com/example/orderlythreads/Inventory.kt
+++ app/src/main/java/com/example/orderlythreads/Inventory.kt
@@ -1,12 +1,13 @@
 package com.example.orderlythreads
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Inventory : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +16,24 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val tab1 = findViewById<TextView>(R.id.tab1)
+        val tab2 = findViewById<TextView>(R.id.tab2)
+        val tab3 = findViewById<TextView>(R.id.tab3)
+        val tab4 = findViewById<TextView>(R.id.tab4)
+
+        val tabs =  listOf(tab1, tab2, tab3, tab4)
+
+        //loop that handles selection of each tab
+        tabs.forEach { tab ->
+            tab.setOnClickListener{
+
+                tabs.forEach { it.isSelected  = false } //deselects the current tab open
+
+                tab.isSelected = true //highlights the selected tab
+            }
+
         }
     }
 }
