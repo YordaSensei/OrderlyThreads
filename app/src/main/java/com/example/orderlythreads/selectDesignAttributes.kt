f39234e21332b4064d6f23b75e8a8e4b351db248
SamSL1212 <sanluissam05@gmail.com>
2025-11-22 11:37:41 +0800

    Add Order and Edit Order Page Added (Unfinished)

--- app/src/main/java/com/example/orderlythreads/selectDesignAttributes.kt
+++ app/src/main/java/com/example/orderlythreads/selectDesignAttributes.kt
@@ -1,20 +1,44 @@
 package com.example.orderlythreads
 
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.animation.LayoutTransition
+import androidx.core.view.get
+import androidx.transition.AutoTransition
+import androidx.transition.TransitionManager
+import androidx.core.view.isGone
+
 
 class selectDesignAttributes : AppCompatActivity() {
+    private lateinit var upperWearInfo: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_select_design_attributes)
+
+        upperWearInfo = findViewById(R.id.upperWearLayout)
+
+        upperWearInfo.layoutTransition.enableTransitionType(LayoutTransition.CHANGING)
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
     }
+    fun expandUpperWear(view: View) {
+        val showUpperWear = if (upperWearInfo.isGone) View.VISIBLE else View.GONE
+
+        TransitionManager.beginDelayedTransition(upperWearInfo, AutoTransition())
+        upperWearInfo.visibility = showUpperWear
+    }
+
 }
