f2e9cd0fbeb274ea14bf13ae0aef042def90d936
Jerp010 <jeffmarquez2006@gmail.com>
2025-12-05 21:02:08 +0800

    made blueprint for popup dialogs
    made mira's approve order to be dynamic and a blueprint

--- app/src/main/java/com/example/orderlythreads/Inventory.kt
+++ app/src/main/java/com/example/orderlythreads/Inventory.kt
@@ -1,6 +1,8 @@
 package com.example.orderlythreads
 
+import android.app.Dialog
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.FrameLayout
@@ -20,7 +22,7 @@ class Inventory : AppCompatActivity() {
     private lateinit var adapter: InventoryAdapter
     private var selectedTab = 0
 
-    // DATA LISTS
+    // region Data Lists
     private val fabricData = mutableListOf(
         InventoryItem("Merino", "20", R.drawable.img_merino),
         InventoryItem("Souffle Yarn", "20", R.drawable.img_scouffle_yarn),
@@ -45,26 +47,37 @@ class Inventory : AppCompatActivity() {
         InventoryItem("Bows", "20", R.drawable.img_bow),
         InventoryItem("Rivets", "20", R.drawable.img_pearls)
     )
+    // endregion
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.inventory)
+        
+        setupWindowInsets()
+        setupTabs()
+        setupRecyclerView()
+        
+        // Set initial state
+        findViewById<TextView>(R.id.tab1).isSelected = true
+        updateDataForSelectedTab()
+    }
 
+    private fun setupWindowInsets() {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
 
-        // TABS
+    private fun setupTabs() {
         val tab1 = findViewById<TextView>(R.id.tab1)
         val tab2 = findViewById<TextView>(R.id.tab2)
         val tab3 = findViewById<TextView>(R.id.tab3)
         val tab4 = findViewById<TextView>(R.id.tab4)
         val addButton = findViewById<FrameLayout>(R.id.addButton)
-
-
+        
         val tabs = listOf(tab1, tab2, tab3, tab4)
 
         tabs.forEachIndexed { index, tab ->
@@ -75,72 +88,21 @@ class Inventory : AppCompatActivity() {
                 updateDataForSelectedTab()
             }
         }
-
-        // RECYCLER + ADAPTER
-        val recycler = findViewById<RecyclerView>(R.id.inventoryRecycler)
-        adapter = InventoryAdapter(mutableListOf()) { item, position, anchorView ->
-            // Create and show the popup menu
-            val popup = PopupMenu(this, anchorView)
-            popup.menuInflater.inflate(R.menu.inventory_card_menu, popup.menu)
-
-            popup.setOnMenuItemClickListener { menuItem ->
-                when (menuItem.itemId) {
-
-                    R.id.edit -> {
-                        // Open dialog to edit item
-                        showEditDialog(item.name, item.quantity) { newName, newQty ->
-                            item.name = newName
-                            item.quantity = newQty
-                            adapter.notifyItemChanged(position)
-                            Toast.makeText(this, "Item updated", Toast.LENGTH_SHORT).show()
-                        }
-                        true
-                    }
-
-                    R.id.add -> {
-                        // Increment quantity
-                        val currentQty = item.quantity.toIntOrNull() ?: 0
-                        val newQty = currentQty + 1
-                        item.quantity = newQty.toString()
-                        adapter.notifyItemChanged(position)
-                        Toast.makeText(this, "${item.name} quantity: $newQty", Toast.LENGTH_SHORT).show()
-                        true
-                    }
-
-                    R.id.delete -> {
-                        // Confirm before deleting
-                        AlertDialog.Builder(this)
-                            .setTitle("Delete Item")
-                            .setMessage("Are you sure you want to delete \"${item.name}\"?")
-                            .setPositiveButton("Delete") { _, _ ->
-                                adapter.items.removeAt(position)
-                                adapter.notifyItemRemoved(position)
-                                Toast.makeText(this, "${item.name} deleted", Toast.LENGTH_SHORT).show()
-                            }
-                            .setNegativeButton("Cancel", null)
-                            .show()
-                        true
-                    }
-
-                    else -> false
-                }
-            }
-
-            popup.show()
-        }
-        recycler.adapter = adapter
-        recycler.layoutManager = GridLayoutManager(this, 2)
-
-        // ADD BUTTON
+        
         addButton.setOnClickListener {
             showAddItemDialog()
         }
-
-
-        // DEFAULT LOAD
-        tabs[0].isSelected = true
-        updateDataForSelectedTab()
     }
+
+    private fun setupRecyclerView() {
+        val recycler = findViewById<RecyclerView>(R.id.inventoryRecycler)
+        adapter = InventoryAdapter(mutableListOf()) { item, position, anchorView ->
+            showItemMenu(item, position, anchorView)
+        }
+        recycler.adapter = adapter
+        recycler.layoutManager = GridLayoutManager(this, 2)
+    }
+
     private fun updateDataForSelectedTab() {
         when (selectedTab) {
             0 -> adapter.updateData(fabricData)
@@ -150,39 +112,80 @@ class Inventory : AppCompatActivity() {
         }
     }
 
+    private fun showItemMenu(item: InventoryItem, position: Int, anchorView: View) {
+        val popup = PopupMenu(this, anchorView)
+        popup.menuInflater.inflate(R.menu.inventory_card_menu, popup.menu)
+
+        popup.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.edit -> {
+                    showEditDialog(item.name, item.quantity) { newName, newQty ->
+                        item.name = newName
+                        item.quantity = newQty
+                        adapter.notifyItemChanged(position)
+                        Toast.makeText(this, "Item updated", Toast.LENGTH_SHORT).show()
+                    }
+                    true
+                }
+                R.id.add -> {
+                    val currentQty = item.quantity.toIntOrNull() ?: 0
+                    val newQty = currentQty + 1
+                    item.quantity = newQty.toString()
+                    adapter.notifyItemChanged(position)
+                    Toast.makeText(this, "${item.name} quantity: $newQty", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.delete -> {
+                    showDeleteConfirmationDialog(item, position)
+                    true
+                }
+                else -> false
+            }
+        }
+        popup.show()
+    }
 
     private fun showAddItemDialog() {
         val dialogView = layoutInflater.inflate(R.layout.dialog_add_item, null)
         val itemName = dialogView.findViewById<EditText>(R.id.editItemName)
         val itemQuantity = dialogView.findViewById<EditText>(R.id.editItemQuantity)
         val selectImageButton = dialogView.findViewById<Button>(R.id.selectImageButton)
+        val btnAddItem = dialogView.findViewById<Button>(R.id.btn_add_item)
+        val btnCancelAddItem = dialogView.findViewById<Button>(R.id.btn_cancel_add_item)
+
+        val dialog = Dialog(this)
+        dialog.setContentView(dialogView)
+        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        dialog.setCancelable(true)
 
         selectImageButton.setOnClickListener {
             Toast.makeText(this, "Image selection is not implemented yet", Toast.LENGTH_SHORT).show()
         }
 
-        AlertDialog.Builder(this)
-            .setTitle("Add New Item")
-            .setView(dialogView)
-            .setPositiveButton("Add") { _, _ ->
-                val name = itemName.text.toString()
-                val quantity = itemQuantity.text.toString()
+        btnAddItem.setOnClickListener { _ ->
+            val name = itemName.text.toString()
+            val quantity = itemQuantity.text.toString()
 
-                if (name.isNotEmpty() && quantity.isNotEmpty()) {
-                    val newItem = InventoryItem(name, quantity, R.drawable.img_placeholder)
-                    when (selectedTab) {
-                        0 -> fabricData.add(newItem)
-                        1 -> colorData.add(newItem)
-                        2 -> closuresData.add(newItem)
-                        3 -> accentsData.add(newItem)
-                    }
-                    updateDataForSelectedTab()
-                } else {
-                    Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show()
+            if (name.isNotEmpty() && quantity.isNotEmpty()) {
+                val newItem = InventoryItem(name, quantity, R.drawable.img_placeholder)
+                when (selectedTab) {
+                    0 -> fabricData.add(newItem)
+                    1 -> colorData.add(newItem)
+                    2 -> closuresData.add(newItem)
+                    3 -> accentsData.add(newItem)
                 }
+                updateDataForSelectedTab()
+                dialog.dismiss()
+            } else {
+                Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show()
             }
-            .setNegativeButton("Cancel", null)
-            .show()
+        }
+
+        btnCancelAddItem.setOnClickListener { _ ->
+            dialog.dismiss()
+        }
+
+        dialog.show()
     }
 
     private fun showEditDialog(
@@ -213,4 +216,17 @@ class Inventory : AppCompatActivity() {
             .show()
     }
 
+    private fun showDeleteConfirmationDialog(item: InventoryItem, position: Int) {
+        AlertDialog.Builder(this)
+            .setTitle("Delete Item")
+            .setMessage("Are you sure you want to delete \"${item.name}\"?")
+            .setPositiveButton("Delete") { _, _ ->
+                adapter.items.removeAt(position)
+                adapter.notifyItemRemoved(position)
+                Toast.makeText(this, "${item.name} deleted", Toast.LENGTH_SHORT).show()
+            }
+            .setNegativeButton("Cancel", null)
+            .show()
+    }
+    // endregion
 }
